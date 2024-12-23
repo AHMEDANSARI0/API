ebf38e179417a5257d2978300f70ef517bce9dda
AHMEDANSARI0 <ahmedansari1552003@gmail.com>
2024-12-23 12:18:37 +0500

    first commit

---
+++ src/app/components/ChildComponent.tsx
@@ -0,0 +1,14 @@
+
+//   const ChildComponent = (props: any) => {
+//     console.log(props) 
+//     return (
+//         <div className="border p-7 w-9/12 ">
+//             <h1 className="text-black text-center font-bold">
+//                 {props.name} 
+
+//             </h1>
+//         </div>
+//     )
+//   };
+
+//   export default ChildComponent
